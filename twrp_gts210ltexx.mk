af36f3a780a68837bf046b0eed5d97df52b6319e
Dr droid <yeswhynot69@outlook.com>
2024-08-10 19:19:56 +0530

    omni to twrp

--- twrp_gts210ltexx.mk
+++ twrp_gts210ltexx.mk
@@ -8,11 +8,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
 
 # Time Zone data for Recovery
-PRODUCT_COPY_FILES += \
-    bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
+#PRODUCT_COPY_FILES += \
+#    bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := gts210ltexx
-PRODUCT_NAME := omni_gts210ltexx
+PRODUCT_NAME := twrp_gts210ltexx
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
